ad65418620472865c9da3e4bd9351d9664411c61
CannibalCereal <joshcho555@gmail.com>
2018-07-11 14:38:20 -0700

    set up routes for landing page, signup, and login

---
+++ app/controllers/signups_controller.rb
@@ -0,0 +1,20 @@
+class SignupsController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(signup_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/landing'
+    else
+      flash[:notice] = "Could not create user. Please try again."
+      redirect_to '/signups/new'
+    end
+  end
+
+  private
+  def signup_params
+    params.require(:user).permit(:email, :name, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/landing' => 'pages#landing'
+  resources 'signups'
+  post 'login' => 'sessions#create'
+  get '/login' => 'sessions#new'
+  delete 'logout' => 'sessions#destroy'
+  
 end
