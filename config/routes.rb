0552dbf7df85a320adb98a5550212f5adecadc9d
John <herezjonnie@gmail.com>
2018-07-12 15:04:42 -0700

    Using localtower to deal with backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  if Rails.env.development? and defined?(Localtower)
+    mount Localtower::Engine, at: "localtower"
+  end
+
   root 'pages#home'
   get '/home' => 'pages#home'
   get '/landing' => 'pages#landing'
