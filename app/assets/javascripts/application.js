17f5b525f5934a34b956d838931e6b88a163d390
CannibalCereal <joshcho555@gmail.com>
2018-07-19 14:10:53 -0700

    stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,34 +21,59 @@
 $(document).ready(function() {
   $('#homepageCalendar').fullCalendar({
     header: { left: 'prev,today,next',
-              center: 'title',
-              right: 'agendaWeek,month'
-            },
-    editable: false, // Don't allow editing of events
-    handleWindowResize: true
-  })
+    center: 'title',
+    right: 'agendaWeek,month'
+  },
+  editable: false, // Don't allow editing of events
+  handleWindowResize: true
+})
 });
 
 $(function() {
   $('#createEventCalendar').fullCalendar({
     header: { left: 'prev,today,next',
-              center: 'title',
-              right: 'agendaWeek,month'
-            },
-    handleWindowResize: true,
-    defaultView: 'agendaWeek',
-    editable: true,
-    selectable: true,
-    selectOverlap: false,
-    select: function (start, end, jsEvent, view) {
-      $("#createEventCalendar").fullCalendar('addEventSource', [{
-        start: start,
-        end: end,
-      }, ]);
-      $("#createEventCalendar").fullCalendar("unselect");
-    },
-     eventClick: function (event, jsEvent, view) {
-       $('#createEventCalendar').fullCalendar('removeEvents', event._id);
-    },
-  })
+    center: 'title',
+    right: 'agendaWeek,month'
+  },
+  contentHeight:600,
+  eventColor: '#624763',
+  handleWindowResize: true,
+  defaultView: 'agendaWeek',
+  eventDurationEditable: true,
+  selectable: true,
+  selectOverlap: false,
+  select: function (start, end, jsEvent, view) {
+    $("#createEventCalendar").fullCalendar('addEventSource', [{
+      start: start,
+      end: end,
+    }, ]);
+    $("#createEventCalendar").fullCalendar("unselect");
+  },
+  //eventClick: function (event, jsEvent, view) {
+  //  $('#createEventCalendar').fullCalendar('removeEvents', event._id);
+  //},
+  /*eventRender: function(event, element) {
+  element.find(".fc-bg").css("pointer-events","none");
+  element.append("<span id='btnDeleteEvent'>X</span>" );
+  element.find("#btnDeleteEvent").click(function(){
+  $('#createEventCalendar').fullCalendar('removeEvents',event._id);
+});
+},*/
+eventMouseover:function(event,domEvent,view){
+
+  var el=$(this);
+
+  var layer='<div id="events-layer" class="fc-transparent"><span id="delbut'+event.id+'" style="width:10%;" class="btn btn-default trash btn-xs">X</span></div>';
+  el.append(layer);
+
+  el.find(".fc-bg").css("pointer-events","none");
+
+  $("#delbut"+event.id).click(function(){
+    $('#createEventCalendar').fullCalendar('removeEvents', event._id);
+  });
+},
+eventMouseout:function(event){
+  $("#events-layer").remove();
+},
+})
 });
